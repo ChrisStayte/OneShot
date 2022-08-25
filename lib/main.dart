0d1ed4924a12d74d78fac2ffd9ff8e6762087a6c
Chris <ctstayte@gmaill.com>
2022-08-24 21:08:42 -0400

    Added Settings

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'One Shot',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
